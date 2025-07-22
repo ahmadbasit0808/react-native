a64ee4441155d0f627274d195067ce31b86596ae
Muhammad Ahmad Basit <162097637+ahmadbasit0808@users.noreply.github.com>
2025-07-22 20:42:05 +0500

    Update App.js

--- App.js
+++ App.js
@@ -6,18 +6,21 @@ This component showcase use of following components
 such as headerstyle and contentstyle
 4.Screen (name of screen that is passed to navigate() , options={{title:""}} to set a different title)
  options={{ headerShown: false }}) to hide the title , component: the component to be rendered
+5.Header stylings (headerStyle to style the box of header, headertintcolor for color of header text
+headertitlestyle for style of header text, headertitle and headerright takes an arrow function and 
+return some component)
 */
 
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
-import { useColorScheme } from "react-native";
+import { useColorScheme, Image } from "react-native";
 import { getThemedBg } from "./utils/colorSchemes";
 import Login from "./components/Login";
 import MenuItems from "./components/SectionList";
 import WelcomeScreen from "./components/WelcomeScreen";
 import Form from "./components/TextInput";
 import MenuItemsFlat from "./components/FlatList";
-
+import logo from "./img/image.png";
 const Stack = createNativeStackNavigator();
 
 export default function App() {
@@ -38,8 +41,6 @@ export default function App() {
               style={{ width: 100, height: 40, resizeMode: "contain" }}
             />
           ),
-          tabBarStyle: { backgroundColor: "#EE9972" },
-          tabBarActiveTintColor: "#111",
           contentStyle: getThemedBg(colorScheme),
         }}
       >
